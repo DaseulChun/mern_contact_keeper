4e0b319493dc6ca4758cff35ee895d1deda88fb6
DaseulChun <nandaseul@gmail.com>
2020-08-10 23:30:11 -0700

    Create and respond with Json Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const { check, validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -44,7 +46,25 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
       await user.save();
 
-      res.send('User saved');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      const jwtKey = process.env.JWT_KEY;
+
+      jwt.sign(
+        payload,
+        jwtKey,
+        {
+          expiresIn: 3600
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
